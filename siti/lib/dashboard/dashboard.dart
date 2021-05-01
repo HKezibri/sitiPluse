c7da1f5aa0ed97612629c3723ce093e6d46d4ffa
Abdellah-05 <abdouelaaroub@gmail.com>
2021-05-01 16:33:50 +0000

    dashboard templaite

---
+++ siti/lib/dashboard/dashboard.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class GaugeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Radial Gauge Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget _getGauge({bool isRadialGauge = true}) {
+    if (isRadialGauge) {
+      return _getRadialGauge();
+    } else {
+      return _getLinearGauge();
+    }
+  }
+
+  Widget _getRadialGauge() {
+    return SfRadialGauge(
+        backgroundColor: Colors.white70,
+        title: GaugeTitle(
+            text: 'test',
+            textStyle:
+            const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+        axes: <RadialAxis>[
+          RadialAxis(minimum: 0, maximum: 100, ranges: <GaugeRange>[
+            GaugeRange(
+                startValue: 0,
+                endValue: 35,
+                color: Colors.green,
+                startWidth: 10,
+                endWidth: 10),
+            GaugeRange(
+                startValue: 35,
+                endValue: 70,
+                color: Colors.orange,
+                startWidth: 10,
+                endWidth: 10),
+            GaugeRange(
+                startValue: 70,
+                endValue: 100,
+                color: Colors.red,
+                startWidth: 10,
+                endWidth: 10)
+          ], pointers: <GaugePointer>[
+            NeedlePointer(value: 50)
+          ], annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+                widget: Container(
+                    child: const Text('50.0',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold))),
+                angle: 90,
+                positionFactor: 0.5)
+          ])
+        ]);
+  }
+
+  Widget _getLinearGauge() {
+    return Container(
+      child: SfLinearGauge(
+          minimum: 0.0,
+          maximum: 100.0,
+          orientation: LinearGaugeOrientation.horizontal,
+          majorTickStyle: LinearTickStyle(length: 20),
+          axisLabelStyle: TextStyle(fontSize: 12.0, color: Colors.black),
+          axisTrackStyle: LinearAxisTrackStyle(
+              color: Colors.cyan,
+              edgeStyle: LinearEdgeStyle.bothFlat,
+              thickness: 15.0,
+              borderColor: Colors.grey)),
+      margin: EdgeInsets.all(10),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Siti')),
+        body: _getGauge());
+  }
+}
--- siti/lib/main.dart
+++ siti/lib/main.dart
@@ -1,11 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:siti/dashboard/dashboard.dart';
+
+void main() {
+  return runApp(GaugeApp());
+}
+
+
+
+/*
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
 
-  final response = await http.get(Uri.http('your addresse', '/'));
+  final response = await http.get(Uri.http('192.168.0.178:5000', '/'));
 
   final decoded = convert.jsonDecode(response.body) as Map<String, dynamic>;
   print("${decoded['status']} <==============");
 }
+
+ */
 
